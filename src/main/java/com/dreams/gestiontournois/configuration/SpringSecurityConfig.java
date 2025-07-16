2aa2f76437fe97449536be1ec276c16aa7304eb0
CheunRamassy <cheunramassy.pr@gmail.com>
2025-07-16 13:46:45 +0400

    Mise à jour de SpringSecurityConfig

--- src/main/java/com/dreams/gestiontournois/configuration/SpringSecurityConfig.java
+++ src/main/java/com/dreams/gestiontournois/configuration/SpringSecurityConfig.java
@@ -1,40 +1,51 @@
 package com.dreams.gestiontournois.configuration;
 
+import com.dreams.gestiontournois.Service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    private final UserService userDetailsServices;
+
+    public SpringSecurityConfig(UserService userDetailsServices ) {
+        this.userDetailsServices = userDetailsServices;
+    }
+
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(auth ->{
-            auth.requestMatchers("/DoubleElimination/admin").hasRole("ADMIN");
-            auth.requestMatchers("/DoubleElimination/user").hasRole("USER");
-            auth.requestMatchers("/Jeux/create").hasRole("ADMIN");
-            auth.requestMatchers("/Jeux/edit").hasRole("ADMIN");
-            auth.requestMatchers("/Jeux/delete").hasRole("ADMIN");
-            auth.requestMatchers("/DoubleElimination/ListeTournois").permitAll();
-            auth.requestMatchers("/DoubleElimination/view/{id}").permitAll();
-            auth.requestMatchers("/DoubleElimination/createTournois").permitAll();
-            auth.requestMatchers("/DoubleElimination/save").permitAll();
-            auth.requestMatchers("/DoubleElimination/editTournoi/{id}").permitAll();
-            auth.requestMatchers("/DoubleElimination/delete/{id}").permitAll();
-            auth.anyRequest().authenticated();
-        }).formLogin(form -> form
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth ->{
+                    auth.requestMatchers("/login","/User/Registration", "/User/saveAccount").permitAll();
+                    auth.requestMatchers("/Jeux/create").hasRole("ADMIN");
+                    auth.requestMatchers("/Jeux/edit").hasRole("ADMIN");
+                    auth.requestMatchers("/Jeux/delete").hasRole("ADMIN");
+                    auth.anyRequest().permitAll();
+        })
+                .formLogin(form -> form
                         .loginPage("/login")
                         .loginProcessingUrl("/login")
                         .defaultSuccessUrl("/Home", true)
+                        .failureUrl("/login?error=true")
                         .permitAll()
                 )
                 .logout(logout -> logout
@@ -42,25 +53,36 @@ public class SpringSecurityConfig {
                         .permitAll()
                 )
                 .build();
-
-
         //                .formLogin(Customizer.withDefaults()).build();
     }
 
+//    @Bean
+//    public UserDetailsService users() {
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("user"))
+//                .roles("USER").build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("USER", "ADMIN").build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
     @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("user"))
-                .roles("USER").build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN").build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsServices);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
  }
